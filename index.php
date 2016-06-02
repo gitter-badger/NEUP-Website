a046182182ed8e927cbfbcbef2b47f989ae2c024
onionwyl <onionwyl@qq.com>
2016-06-02 13:03:16 +0800

    init wordpress theme

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes" />    
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="format-detection" content="telephone=yes"/>
+    <meta name="msapplication-tap-highlight" content="no" />
+    <link href="<?php bloginfo('template_url'); ?>/style/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_url'); ?>/style/global.css" rel="stylesheet">
+<title>无标题文档</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class = "banner">
+
+        </div>
+        <nav class="menubar">
+          <ul class="nav nav-justified">
+            <li class="active"><a href="#" class="menucolor">先锋首页</a></li>
+            <li><a href="#" class="menucolor">东大共青团</a></li>
+            <li><a href="#" class="menucolor">学生组织</a></li>
+            <li><a href="#" class="menucolor">网上团支部</a></li>
+            <li><a href="#" class="menucolor">青年之声</a></li>
+            <li><a href="#" class="menucolor">先锋论坛</a></li>
+            <li><a href="#" class="menucolor">文件下载</a></li>
+            <li><a href="#" class="menucolor">友情链接</a></li>
+          </ul>
+        </nav>
+      </div>
+
+
+      <div class="line1">
+        <div class="line11">
+            11
+        </div>
+      	<div class="line12">
+           12 
+        </div>
+      	<div class="line13">
+           13 
+        </div>     
+        <div class="line14">
+            14
+        </div>        
+      </div>
+
+
+    <div class="line2">
+        <div class="line21">
+            21
+        </div>
+        <div class="line22">
+            22
+        </div>
+        <div class="line23">
+            23
+        </div>
+    </div> <!-- /container -->
+
+        <div class="line3">
+            <div class="line31">
+                31
+            </div>
+            <div class="line32">
+                32
+            </div>
+            <div class="line33">
+                33
+            </div>
+            <div class="line34">
+                34
+            </div>
+            <div class="line35">
+                35
+            </div>
+        </div>
+
+
+    <!-- </div> -->
+    <footer class="footer">
+        <p>&copy; 2016 Company, Inc.</p>
+      </footer>
+</body>
+</html>
